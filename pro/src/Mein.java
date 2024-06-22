e945d247741b447713494e4cde36bfd97d44359e
mihan <maleksyutin@bk.ru>
2024-06-22 22:11:05 +0300

    Последнее изминение

--- pro/src/Mein.java
+++ pro/src/Mein.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Mein {
     public static void main(String[] args) {
 
-        System.out.println("Это матиматика!");
+        System.out.println(".");
         System.out.println("Точная наука");
         System.out.println("Точная наука");
         System.out.println("Это матиматика!");
