1b4aed990769bb7fe584490c1b323dba5a2e7799
mihan <maleksyutin@bk.ru>
2024-06-22 20:30:14 +0300

    Я добавол много всякого а он не сохранил

--- pro/src/Mein.java
+++ pro/src/Mein.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Mein {
     public static void main(String[] args) {
         System.out.println("hello Man");
-
+        System.out.println("Введите первое число:");
+        int nam1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int nam2 = new Scanner(System.in).nextInt();
+        System.out.println("Введите третье число:");
+        int nam3 = new Scanner(System.in).nextInt();
+        System.out.println(nam1+nam2+nam3);
     }
 
 }
