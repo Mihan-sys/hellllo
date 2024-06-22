4ca3c61747e1742d1864cc57e554cc70de5d4840
mihan <maleksyutin@bk.ru>
2024-06-22 20:49:11 +0300

    jldfbnflnadkbfdbla;mfad;blm\

--- pro/src/Mein.java
+++ pro/src/Mein.java
@@ -12,6 +12,14 @@ public class Mein {
         System.out.println("Это матиматика!");
         System.out.println("Точная наука");
         System.out.println("Точная наука");
+        System.out.println("Введите первое число:");
+        int nam1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int nam2 = new Scanner(System.in).nextInt();
+        System.out.println("Введите третье число:");
+        int nam3 = new Scanner(System.in).nextInt();
+        System.out.println(nam1+nam2+nam3);
+        System.out.println("Точная наука");
 
     }
 
